b15436affb2be557746d9625cc25fefb887e75d3
alikiminza <ali.kiminza@student.moringaschool.com>
2023-09-12 11:30:20 +0300

    final changes

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id Integer primary_key,
+    name Text,
+    age Integer,
+    sex Text,
+    color Text,
+    temperament Text,
+    alive Integer
+)
---
+++ lib/insert.sql
@@ -0,0 +1,26 @@
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "Mr. Chocolate", 3, "M", "Chocolate", "social", 1
+);
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "Rowdy", 5, "M", "White", "affectionate", 1
+);
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "Tabitha", 7, "F", "Yellow", "fearful", 1
+);
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "Sergeant Brown", 2, "M", "Chocolate", "strict", 1
+);
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "Melissa", 6, "F", "Brown", "affectionate", 1
+);
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "Grinch", 1, "M", "White", "social", 1
+);
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "Wendy", 9, "F", "Black", "playfull", 1
+);
+INSERT INTO bears (name,age, sex,color,temperament,alive) VALUES (
+    "NULL", 8, "M", "Brown", "social", 1
+);
+
+
